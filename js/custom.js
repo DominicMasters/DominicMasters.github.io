eec72a74dd539fe7e571b7531a74dd36ee4ef6ed
Dominic Masters <dominic.masters@mail.com>
2017-01-01 19:40:48 +0000

    IMG test_

--- js/custom.js
+++ js/custom.js
@@ -97,15 +97,7 @@ var customScripts = {
     }
 }
 $('document').ready(function () {
-	 $.backstretch([
-   "./images/IMG0102.jpg"
-   , "./images/IMG1998.jpg"
-   , "./images/IMG1541.jpg"
-   , "./images/IMG1353.jpg"
-   , "./images/IMG1997.jpg"
-   , "./images/IMG1442.jpg"
-   , "./images/IMG0512.jpg"
-   , "./images/IMG0627.jpg"
+	 $.backstretch(["./images/flowers.jpg"
   ], {duration: 3000, fade: 1250});
 
     customScripts.init();
